3739e900a65607c2491763cb9ae9bbdd0e5943fa
Jayme Woodfill <jayme.woodfill@gmail.com>
2022-02-19 11:53:09 -0600

    Adding the React and Bootstrap
    made this in a different area so I'm adding it to the git so it updates

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <div className="App" background='#FF7043'>
+      <header className="App-header">
+        <Button>Login</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
