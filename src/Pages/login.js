da1e1ea1452a0c17932e1aa078a4f5f06644ca57
Jayme Woodfill <jayme.woodfill@gmail.com>
2022-02-23 09:44:36 -0600

    Update login.js
    trying to figure out why bootstrap isnt working

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -5,25 +5,28 @@ function login(){
     return(
         <>
             <Container>
-                <h1 className="shadow-sm text-success mt-5 p-3 text-center rounded">Admin Login</h1>
-                <Row className="mt-5">
-                    <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Email Address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter Email"/>
-                            </Form.Group>
+                <Row>
+                    <Col lg={4} md={6} sm={12}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password"/>
-                            </Form.Group>
-                        </Form>
-
-                        <Button variant="success btn-block" type="submit">
-                            Login
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
                         </Button>
-
+                        </Form>
                     </Col>
                 </Row>
             </Container>
