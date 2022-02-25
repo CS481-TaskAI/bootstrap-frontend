ba7d7b9340e8cd3abc3bdf0975e8786d6d9dfa77
Natalia Gonzalez <natalia.gonzalez438@gmail.com>
2022-02-24 18:56:48 -0800

    creating home page

---
+++ src/Pages/Home/home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function home(){
+    /*
+        want to add an <img> for the users porfile pic
+        but probably won't have time to add that
+        in the welcome-user-container, add the welcome user greeting with the user's name
+            - will add that later once I figure out the attributes for user information
+    */
+    return(
+        <div className="content-container">
+            <div className="navbar-container">
+                <div className="welcome-user-container">
+                    
+                </div>
+
+                <div className="create-team-button">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default home;
