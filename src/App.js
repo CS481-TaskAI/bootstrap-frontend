72747d970c6bbb67f5b63ed4be80c08f0164feaf
Jayme Woodfill <jayme.woodfill@gmail.com>
2022-02-28 10:24:29 -0600

    Babel isnt running
    for some reason it wont let me run my code due to something about babel? ive reset and tried multiple times and yet nothing is happening

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './Pages/Login';
+import SignUp from './Pages/Signup';
+import Home from './Pages/Home';
+import AccountInfo from './Pages/AccountInfo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<Router>
+          <Routes>
+            <Route exact path='/' element={Login} />
+            <Route path="/sign-in" element={Login} />
+            <Route path="/sign-up" element={SignUp} />
+            <Route path="/accountInfo" element={AccountInfo} />
+            <Route path="/home" element={Home} />
+          </Routes>
+      </Router>
+      
   );
+  
 }
 
 export default App;
---
+++ src/Pages/AccountInfo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AccountInfo() {
+  return (
+    <div>accountInfo</div>
+  );
+}
+
+export default AccountInfo;
