dea315c62f14b7d12f076fbaaf1b1df3486d7744
Natalia Gonzalez <natalia.gonzalez438@gmail.com>
2022-02-28 09:45:05 -0800

    .

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ import AccountInfo from './Pages/accountInfo';
 
 function App() {
   return (
-  <div>
+        <div>
           <Routes>
-            <Route exact path='/' element={Login} />
-            <Route path="/sign-in" element={Login} />
-            <Route path="/sign-up" element={SignUp} />
-            <Route path="/accountInfo" element={AccountInfo} />
-            <Route path="/home" element={Home} />
+            <Route exact path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/accountInfo" element={<AccountInfo />} />
           </Routes>
-  </div>
+        </div>
   );
   
 }
