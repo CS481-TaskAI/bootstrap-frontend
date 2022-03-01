32cedefa224d77194b74c293eecf0745e3746ddb
Jayme Woodfill <jayme.woodfill@gmail.com>
2022-03-01 08:43:49 -0600

    Natalia and useState
    I added Natalias page, and the useState-- i THINK

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,136 +1,50 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Col, Container, Nav, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, ListGroup, Nav, Row } from "react-bootstrap";
 
 
-function WelcomeUser()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Row className="d-flex flex-row">
-                <Col className="justify-content-center">
-                    <h1>Welcome, User</h1>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+
+
 
 /*
     container for the create team button, need to add an
     onClick function and create another component for the 
     create team form 
 */
-function CreateTeamButton()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Button className="bg-light" variant="outline-secondary" size="sm">
-                Create Team
-            </Button>   
-        </Container>
-    );
 
-}
 
 /*
     container for the nav bar side of the header, don't need to add much
     other than changing the color of the font to indicate the active page
     the user is currently on.
 */
-function NavBar()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Row className="d-flex flex-row justify-content-center">
-                <Nav>
-                    <Nav.Item>
-                        <Nav.Link href="/home">Home</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/projects">Projects</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/contacts">Contacts</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/accountInfo">Account Info</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Row>
-        </Container>
-    );
-}
+
 
 /*
     container for the header bar which holds the welcome user, create team button,
     and nav bar containers and puts them together into one unit.
     will format after seeing how the bootstrap put them together
+
     adding a props to function so that we can pull from the database to 
     display the user's name for the welcome greeting
 */
-function HeaderBar()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Row className="d-flex flex-row justify-content-between">
-                <Col className="position-static d-flex flex-column">
-                    <WelcomeUser/>
-                </Col>
-                <Col className="position-static d-flex flex-column">
-                    <CreateTeamButton/>
-                </Col>
-                <Col className="position-static d-flex flex-column">
-                    <NavBar/>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+
 
 /*
     container for the current date
     will need to format is differently to 
     match up to Taban's prototype in figma
 */
-function FormatDate(date)
-{
-    return date.toLocaleDateString();
-}
-
 
 
 /*
     container for the adding the task component. When user
     clicks on the input form and popup form will pop up and
     allow the user to add tasks to their task category
+
     use an input group to be the popup screen
 */
-function CreateTasks()
-{
-    return(
-        <Container>
-            <Row>
-                <Col>
-                    
-                </Col>
 
-                <Col>
-                    <div>
-                        Or
-                    </div>
-                </Col>
-
-                <Col>
-                    <Button variant="primary">
-                        Primary
-                    </Button>
-                </Col>
-            </Row>
-
-        </Container>
-    );
-}
 
 /*
     container that will hold the task categories
@@ -138,14 +52,7 @@ function CreateTasks()
     boostrap cards. the cards will respresent the categories
     that will hold the tasks
 */
-function TaskCategories()
-{
-    return(
-        <Container>
 
-        </Container>
-    );
-}
 /*
     home will show all of the components inside of the main 
     container. Home container will hold all of the content.
@@ -156,25 +63,113 @@ function Home(){
         button and stuff just to get an idea
         of how we will connect the backend with
         the frontend
+
         Will ask Jayme and backend team about how to go
         about setting up the buttons, inputs, and forms.
     */
     return(
         <>
-            <Container className="bg-white rounded">
-                <Row className="bg-transparent">
-                    <HeaderBar/>
+            <Container fluid="md" className="bg-white rounded mt-5 h-75">
+                <Row className="d-flex justify-content-between mt-4">
+                    <Col className="mt-3 p-3 text-center">
+                        <h4 className="text-dark ">
+                            Welcome, User
+                        </h4>
+                    </Col>
+                    <Col className="mt-3 p-3 text-center">
+                        <Button className="bg-light text-dark btn-sm border-dark rounded-pill">
+                            Create Team
+                        </Button>   
+                        
+                    </Col>
+                    <Col className="mt-3 p-3 text-center">
+                        <Nav>
+                            <Nav.Item>
+                                <Nav.Link className="text-secondary ative" aria-current="page" href="/home">Home</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link className="text-dark" href="/projects">Projects</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link className="text-dark" href="/contacts">Contacts</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link className="text-dark" href="/accountInfo">Account Info</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
                 </Row>
-                <Row>
-                    <FormatDate/>
+
+                <Row className="d-flex justify-content-start text-start">
+                    <Col className="col-3 text-center">
+                        <h6 className="text-body">
+                            Today: March 1, 2022
+                        </h6>
+                    </Col>
                 </Row>
-                <Row>
-                    <CreateTasks/>
+
+                <Row className="d-flex justify-content-between text-start p-3">
+                    <Col className="col-5">
+                        <Form>
+                            <Form.Group controlId="formInputTask">
+                                <Form.Control type="input" placeholder="What would you like to do?" />
+                            </Form.Group>
+                        </Form>
+                    </Col>
+
+                    <Col>
+                        <h6 className="text-dark">
+                            Or
+                        </h6>
+                    </Col>
+
+                    <Col>
+                        <Button className="bg-primary btn-sm rounded-pill">
+                            Add Task Category
+                        </Button>
+                    </Col>
+
+                    <Col>
+                       <Card className="text-white bg-danger rounded">
+                            <Card.Body>
+                                <Card.Title>Weekly Rewards</Card.Title>
+                                <Card.Text> Walk/Gym </Card.Text>    
+                            </Card.Body>   
+                        </Card>  
+                    </Col>
                 </Row>
-                <Row>
-                    <TaskCategories/>
+
+                <Row className="d-flex justify-content-start mt-2">
+                    <Col className="col-2 text-center">
+                        <h6 className="text-body">
+                            Task Overview
+                        </h6>
+                    </Col>
                 </Row>
-                
+
+                <Row className="justify-content-center ms-3">
+                    <Container>
+                        <Row className="mb-3">
+                            <Col className="col-3">
+                                <Card className="shadow-sm text-dark bg-white rounded">
+                                    <Card.Body>
+                                        <Card.Title>To Do</Card.Title>
+                                        <Card.Text>
+                                            <ListGroup>
+                                                <ListGroup.Item>
+                                                    Create tasks
+                                                </ListGroup.Item>
+                                                <ListGroup.Item>
+                                                    Create tasks in different categories
+                                                </ListGroup.Item>
+                                            </ListGroup>
+                                        </Card.Text>    
+                                    </Card.Body>   
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Row>
             </Container>
         </>
     );
--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import { Col, Container, Form, Row, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validateForm(){
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+    }
     return(
         <>
             <Container className="bg-danger rounded">
                 <h1 className="shadow-sm text-white mt-5 p-3 text-center rounded">Task AI</h1>
                 <Row className="mt-5">
                     <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg text-center">
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group controlId="email">
                                 <Form.Label>Email Address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter Email"/>
+                                <Form.Control
+                                autoFocus
+                                type="email"
+                                value={email}
+                                placeholder="Enter Email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                />
                             </Form.Group>
 
-                            <Form.Group controlId="formBasicPassword">
+                            <Form.Group controlId="password">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password"/>
+                                <Form.Control
+                                type="password"
+                                value={password}
+                                placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                />
                             </Form.Group>
                         </Form>
 
-                        <Button className="mt-4"variant="info btn-block" type="submit">
+                        <Button className="mt-4"variant="info btn-block" type="submit" disabled={!validateForm()}>
                             <Link to="/home" className="text-white">Login</Link>
                         </Button>
 
