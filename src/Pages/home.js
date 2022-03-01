8dd5f1a6728ef307cfa1c8a8f7bc29ea258fe450
Natalia Gonzalez <natalia.gonzalez438@gmail.com>
2022-03-01 00:35:43 -0800

    putting everything in the return function

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -3,64 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button, Col, Container, Nav, Row } from "react-bootstrap";
 
 
-function WelcomeUser()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Row className="d-flex flex-row">
-                <Col className="justify-content-center">
-                    <h1>Welcome, User</h1>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+
 
 /*
     container for the create team button, need to add an
     onClick function and create another component for the 
     create team form 
 */
-function CreateTeamButton()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Button className="bg-light" variant="outline-secondary" size="sm">
-                Create Team
-            </Button>   
-        </Container>
-    );
 
-}
 
 /*
     container for the nav bar side of the header, don't need to add much
     other than changing the color of the font to indicate the active page
     the user is currently on.
 */
-function NavBar()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Row className="d-flex flex-row justify-content-center">
-                <Nav>
-                    <Nav.Item>
-                        <Nav.Link href="/home">Home</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/projects">Projects</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/contacts">Contacts</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/accountInfo">Account Info</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Row>
-        </Container>
-    );
-}
+
 
 /*
     container for the header bar which holds the welcome user, create team button,
@@ -70,35 +27,13 @@ function NavBar()
     adding a props to function so that we can pull from the database to 
     display the user's name for the welcome greeting
 */
-function HeaderBar()
-{
-    return(
-        <Container className="d-flex p-0">
-            <Row className="d-flex flex-row justify-content-between">
-                <Col className="position-static d-flex flex-column">
-                    <WelcomeUser/>
-                </Col>
-                <Col className="position-static d-flex flex-column">
-                    <CreateTeamButton/>
-                </Col>
-                <Col className="position-static d-flex flex-column">
-                    <NavBar/>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+
 
 /*
     container for the current date
     will need to format is differently to 
     match up to Taban's prototype in figma
 */
-function FormatDate(date)
-{
-    return date.toLocaleDateString();
-}
-
 
 
 /*
@@ -108,31 +43,7 @@ function FormatDate(date)
 
     use an input group to be the popup screen
 */
-function CreateTasks()
-{
-    return(
-        <Container>
-            <Row>
-                <Col>
-                    
-                </Col>
 
-                <Col>
-                    <div>
-                        Or
-                    </div>
-                </Col>
-
-                <Col>
-                    <Button variant="primary">
-                        Primary
-                    </Button>
-                </Col>
-            </Row>
-
-        </Container>
-    );
-}
 
 /*
     container that will hold the task categories
@@ -140,14 +51,7 @@ function CreateTasks()
     boostrap cards. the cards will respresent the categories
     that will hold the tasks
 */
-function TaskCategories()
-{
-    return(
-        <Container>
 
-        </Container>
-    );
-}
 /*
     home will show all of the components inside of the main 
     container. Home container will hold all of the content.
@@ -165,19 +69,13 @@ function Home(){
     return(
         <>
             <Container className="bg-white rounded">
-                <Row className="bg-transparent">
-                    <HeaderBar/>
+                <Row className="row-cols-3 row-cols-lg-5 g-2 g-lg-3 p-3">
+                    <Col>
+                        <h3 className="text-center text-dark mt-3 p-3">
+                            Welcome, User
+                        </h3>
+                    </Col>
                 </Row>
-                <Row>
-                    <FormatDate/>
-                </Row>
-                <Row>
-                    <CreateTasks/>
-                </Row>
-                <Row>
-                    <TaskCategories/>
-                </Row>
-                
             </Container>
         </>
     );
