f9b6d0cfa10ed0cf6aa76c10352485de2e80c56f
Cesar <ceezy94@gmail.com>
2022-03-01 22:46:25 -0800

    imported task container

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button, Card, Col, Container, Form, ListGroup, Nav, Row } from "react-bootstrap";
-
+import TaskContainer from "../Components/TaskContainer"
 
 
 
