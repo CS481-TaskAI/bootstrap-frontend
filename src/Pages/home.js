1b2893726841c17fd8e29e47adec8dcd2af602da
Natalia Gonzalez <natalia.gonzalez438@gmail.com>
2022-03-05 13:15:57 -0800

    going to figure out fetch calls and such

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,167 +1,139 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link, Route, useNavigate, useLocation } from "react-router-dom";
 import { Button, Card, Col, Container, Form, ListGroup, Nav, Row } from "react-bootstrap";
 import TaskContainer from "../Components/TaskContainer"
+import ProjectContainer from "../Components/ProjectContainer"
 
-
-
-
-/*
-    container for the create team button, need to add an
-    onClick function and create another component for the 
-    create team form 
-*/
-
-
-/*
-    container for the nav bar side of the header, don't need to add much
-    other than changing the color of the font to indicate the active page
-    the user is currently on.
-*/
-
-
-/*
-    container for the header bar which holds the welcome user, create team button,
-    and nav bar containers and puts them together into one unit.
-    will format after seeing how the bootstrap put them together
-
-    adding a props to function so that we can pull from the database to 
-    display the user's name for the welcome greeting
-*/
-
-
-/*
-    container for the current date
-    will need to format is differently to 
-    match up to Taban's prototype in figma
-*/
-
-
-/*
-    container for the adding the task component. When user
-    clicks on the input form and popup form will pop up and
-    allow the user to add tasks to their task category
-
-    use an input group to be the popup screen
-*/
-
-
-/*
-    container that will hold the task categories
-    need to make another function for the 
-    boostrap cards. the cards will respresent the categories
-    that will hold the tasks
-*/
-
-/*
-    home will show all of the components inside of the main 
-    container. Home container will hold all of the content.
-*/
-function Home(){
+function Home(props){
+    
+    const location = useLocation();
     /*
-        adding dummy data to some of the 
-        button and stuff just to get an idea
-        of how we will connect the backend with
-        the frontend
+        HOW TO ACCESS THE VALUES OF THE USER
 
-        Will ask Jayme and backend team about how to go
-        about setting up the buttons, inputs, and forms.
+        location.state.*
+        
+        USER HAS THE FOLLOWING VALUES
+
+        username, email, id, bio
+
+        EXAMPLE: location.state.username
+
     */
-    return(
-        <>
-            <Container fluid="lg" className="bg-white rounded mt-5 h-75">
-                <Row className="d-flex justify-content-between mt-4">
-                    <Col className="col-2 mt-3 p-3 text-center">
-                        <h4 className="text-dark ">
-                            Welcome, User
-                        </h4>
-                    </Col>
-                    <Col className="mt-3 p-3 text-center">
-                        <Button className="bg-light text-dark btn-sm border-dark rounded-pill">
-                            Create Team
-                        </Button>   
-                        
-                    </Col>
-                    <Col className="mt-3 p-3 text-center">
-                        <Nav>
-                            <Nav.Item>
-                                <Nav.Link className="text-secondary active" aria-current="page" href="/home">Home</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link className="text-dark" href="/projects">Projects</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link className="text-dark" href="/contacts">Contacts</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link className="text-dark" href="/accountInfo">Account Info</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Col>
-                </Row>
 
-                <Row className="d-flex justify-content-start mt-2">
-                    <Col className="col-2 text-center">
-                        <h6 className="text-body">
-                            Today: 
-                        </h6>
-                    </Col>
-                </Row>
 
-                <Row className="d-sm-flex p-3">
-                    <Col className="d-grid flex-column col-5">
-                        <Button className="bg-light text-dark border border-secondary btn-sm rounded-pill">
-                            What would you like to do today?
-                        </Button>
-                    </Col>
+        return(
+            <>
+                <Container fluid="lg" className="bg-white rounded mt-5 h-75">
+                    <Row className="d-flex justify-content-between mt-4">
 
-                    <Col className="col-1 align-self-center text-center flex-column">
-                        <h6 className="text-dark">
-                            Or
-                        </h6>
-                    </Col>
+                        <Col className="col-2 mt-3 p-3 text-center">
+                            <h4 className="text-dark ">
+                                Welcome, User
+                            </h4>
+                        </Col>
 
-                    <Col className="col-3 flex-column">
-                        <Button className="bg-primary btn-sm rounded-pill">
-                            Add Task Category
-                        </Button>
-                    </Col>
-                </Row>
+                        <Col className="mt-3 p-3 text-center">
+                            <Button className="bg-light text-dark btn-sm border-dark rounded-pill">
+                                Create Team
+                            </Button>       
+                        </Col>
 
-                <Row className="d-flex justify-content-start mt-2 p-3">
-                    <Col className="col-3 text-center">
-                        <h2 className="text-body">
-                            Task Overview
-                        </h2>
-                    </Col>
-                </Row>
+                        <Col className="mt-3 p-3 text-center">
+                            <Nav>
+                                <Nav.Item>
+                                    <Nav.Link className="text-secondary active" aria-current="page" href="/home">Home</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="text-dark" href="/projects">Projects</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="text-dark" href="/contacts">Contacts</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="text-dark" href="/accountInfo">Account Info</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
 
-                <Row className="justify-content-center ms-3">
-                    <Container>
-                        <Row className="mb-3">
-                            <Col className="col-3">
-                                <Card className="shadow-sm text-dark bg-white rounded">
-                                    <Card.Body>
-                                        <Card.Title className="text-center">To Do</Card.Title>
-                                        <Card.Text>
-                                            <ListGroup>
-                                                <ListGroup.Item>
-                                                    Create tasks
-                                                </ListGroup.Item>
-                                                <ListGroup.Item>
-                                                    Create tasks in different categories
-                                                </ListGroup.Item>
-                                            </ListGroup>
-                                        </Card.Text>    
-                                    </Card.Body>   
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
-                </Row>
-            </Container>
-        </>
-    );
+                    </Row>
+    
+                    <Row className="d-flex justify-content-start mt-2">
+
+                        <Col className="col-2 text-center">
+                            <h6 className="text-body">
+                                Today: 
+                            </h6>
+                        </Col>
+
+                    </Row>
+    
+                    <Row className="d-sm-flex p-3">
+
+                        <Col className="d-grid flex-column col-5">
+                            <Button className="bg-light text-dark border border-secondary btn-sm rounded-pill">
+                                What would you like to do today?
+                            </Button>
+                        </Col>
+    
+                        <Col className="col-1 align-self-center text-center flex-column">
+                            <h6 className="text-dark">
+                                Or
+                            </h6>
+                        </Col>
+    
+                        <Col className="col-3 flex-column">
+                            <Button className="bg-primary btn-sm rounded-pill">
+                                Add Task Category
+                            </Button>
+                        </Col>
+
+                    </Row>
+    
+                    <Row className="d-flex justify-content-start mt-2 p-3">
+
+                        <Col className="col-3 text-center">
+                            <h2 className="text-body">
+                                Task Overview
+                            </h2>
+                        </Col>
+
+                    </Row>
+    
+                    <Row className="justify-content-center ms-3">
+
+                        <Container>
+
+                            <Row className="mb-3">
+
+                                <Col className="col-3">
+
+                                    <Card className="shadow-sm text-dark bg-white rounded">
+                                        <Card.Body>
+                                            <Card.Title className="text-center">To Do</Card.Title>
+                                            <Card.Text>
+                                                <ListGroup>
+                                                    <ListGroup.Item>
+                                                        Create tasks
+                                                    </ListGroup.Item>
+                                                    <ListGroup.Item>
+                                                        Create tasks in different categories
+                                                    </ListGroup.Item>
+                                                </ListGroup>
+                                            </Card.Text>    
+                                        </Card.Body>   
+                                    </Card>
+
+                                </Col>
+
+                            </Row>
+
+                        </Container>
+                    </Row>
+
+                </Container>
+            </>
+        );
 }
 
 export default Home;
