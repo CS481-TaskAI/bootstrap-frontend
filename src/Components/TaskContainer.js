42d366b0116019f53e7033e52b1f4ecbfb1deebb
Cesar <ceezy94@gmail.com>
2022-03-04 19:44:57 -0800

    taskcontainer can take optional project id param

--- src/Components/TaskContainer.js
+++ src/Components/TaskContainer.js
@@ -9,13 +9,22 @@ function TaskContainer(props) {
      // tasks is the variable, setTask is the setter method
     // Not used here, but tasks will be array of task objects
     const [tasks, setTasks] = useState([]);
-    const url = '/tasks'
-    
+    let url = '/tasks'
 
-    // currently just fetching dummmy data
+    if (props.p_id === undefined){
+        url += `?user_id=${props.user_id}`
+    } else {
+        url += `?user_id=${props.user_id}&project_id=${props.p_id}`
+    }
+
+
+
+    console.log(tasks)
+
+    // fetching the data given
     useEffect(() => {
         async function getTasks() {
-            const response = await fetch(`/tasks?user_id=${props.user_id}`, {
+            const response = await fetch(url, {
                     method: 'GET',
                     headers: {
                       'Accept': 'application/json'
@@ -28,30 +37,32 @@ function TaskContainer(props) {
             setTasks(json);
         }
         getTasks();
-      }, [props.user_id]); // Or [] if effect doesn't need props or state
+      }, [props.user_id, props.p_id, url]); // Or [] if effect doesn't need props or state
               // userid will be passed here from props.
 
     return (
-          
-            <div>  {/* Here we will create task cards in a for loop and pass them each a task object */}
-                {tasks.map((task) =>(
-                    <div key={task.id}>
-                        <TaskCard 
-                            id={task.id}
-                            classification={task.classification}
-                            timing={task.timing}
-                            status={task.status}
-                            project_id={task.project_id}
-                            priority={task.priority}
-                            description={task.description}
-                            date_due={task.date_due}
-                            date_assigned={task.date_assigned}
-                        />
-                    </div>
+            <div>
+                {tasks.length > 0 &&
+                    <div>  {/* Here we will create task cards in a for loop and pass them each a task object */}
+                        {tasks.map((task) =>(
+                            <div key={task.id}>
+                                <TaskCard 
+                                    id={task.id}
+                                    classification={task.classification}
+                                    timing={task.timing}
+                                    status={task.status}
+                                    project_id={task.project_id}
+                                    priority={task.priority}
+                                    description={task.description}
+                                    date_due={task.date_due}
+                                    date_assigned={task.date_assigned}
+                                />
+                            </div>
 
-                ))}    
+                        ))}    
+                    </div> 
+                }
             </div>
-          
     );
     
   }
